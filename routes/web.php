9ba5681d026613165f727a641bf58950e2b2f78d
sunzid02 <sunzid02@gmail.com>
2020-09-03 19:34:30 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Route::group(['middleware' => ['auth']], function () {
+//    Route::get('/chat', 'ChatController@index')->name('chat.index');
+//    Route::get('/chat/{id}', 'ChatController@show')->name('chat.show');
+//
+//    Route::post('/chat/get-chat/{id}', 'ChatController@getChat')->name('chat.getChat');
+//
+//
+//});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
